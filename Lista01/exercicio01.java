d9e5f72a6a84e2b284654df0b803d6f0c752349e
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-08-07 22:02:53 -0300

    Create exercicio01.java

---
+++ Lista01/exercicio01.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args)
+        { 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o primeiro lado: ");
+        int x = sc.nextInt();
+        System.out.print("Informe o segundo lado: ");
+        int y = sc.nextInt();
+
+        System.out.println("A area do retangulo é: " + x * y);
+    }
+}
