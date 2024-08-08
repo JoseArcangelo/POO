7ff3f2d7bc718a5b98a1c6d50c2971276ba3cbcf
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-08-07 22:29:04 -0300

    Create exercicio04.java

---
+++ exercicio04.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args)
+        { 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o primeiro lado do triangulo: ");
+        double lado1 = sc.nextDouble();
+
+        System.out.print("Informe o segundo lado do triangulo: ");
+        double lado2 =  sc.nextDouble();
+
+        System.out.print("Informe o angulo entre os lados: ");
+        double angulo = sc.nextDouble();
+        double randius = angulo * (3.14 / 180);
+
+        double area = (lado1 * lado2 * Math.sin(randius)) / 2;
+        System.out.println("Resultado: " + area);
+
+    }
+}
