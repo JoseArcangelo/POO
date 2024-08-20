2cc8492f03157fbd5da0cca51999ec547fd9800c
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-08-20 10:30:06 -0300

    Update and rename Exercicio02.java to Exercicio08.java

--- Lista03/Exercicio08.java
+++ Lista03/Exercicio08.java
@@ -1,7 +1,9 @@
+//Fazer um algoritmo que leia um número inteiro N e escreva a soma dos N primeiros números inteiros
+//positivos. Exemplo: caso seja lido 10 escreve 55. Veja que 1+2+3+4+5+6+7+8+9+10=55
 
 import java.util.Scanner;
 
-public class Exercicio02 {
+public class Exercicio08 {
     public static void main(String[] args)
         {
         Scanner sc = new Scanner(System.in);
