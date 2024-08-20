81eb2d33fab97351839becc6239a7e41df1b6a98
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-08-20 10:31:16 -0300

    Update and rename Exercicio03.java to Exercicio24.java

--- Lista03/Exercicio24.java
+++ Lista03/Exercicio24.java
@@ -1,3 +1,6 @@
+// Fazer um algoritmo que leia números inteiros até que sejam informados dez números perfeitos e escreva o
+// menor número perfeito informado.
+// Um número perfeito é aquele que é igual a soma dos seus divisores. Ex: 6=1+2+3 ; 28=1+2+4+7+14
 public class Exercicio03 {
     public static void main(String[] args)
         {
