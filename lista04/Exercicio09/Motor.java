9025e3f72dfe0beb50df038d1b745daaf59f8255
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-08-28 14:22:06 -0300

    Create Motor.java

---
+++ lista04/Exercicio09/Motor.java
@@ -0,0 +1,14 @@
+public class Motor {
+  String potencia;
+  String tipo_combustivel;
+  String estado;
+  
+  public Motor(String potencia, String tipo_combustivel, String estado){
+    this.potencia = potencia;
+    this.tipo_combustivel = tipo_combustivel;
+    this.estado = estado;
+  }
+
+
+
+}
