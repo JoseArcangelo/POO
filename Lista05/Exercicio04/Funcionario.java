c368b996441e782592f20b59e918be9d541a4af0
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-09-04 10:23:08 -0300

    Create Funcionario.java

---
+++ Lista05/Exercicio04/Funcionario.java
@@ -0,0 +1,36 @@
+public class Funcionario{
+  int codigo;
+  float quantidadeHoras;
+  float valorHora;
+  float valorSalario;
+  float quantidadeHorasExtras;
+
+  public Funcionario(int codigo, float quantidadeHoras, float valorHora, float quantidadeHorasExtras){
+    this.codigo = codigo;
+    this.quantidadeHoras = quantidadeHoras;
+    this.valorHora = valorHora;
+    this.valorSalario = 0;
+    this.quantidadeHorasExtras = quantidadeHorasExtras;
+  }
+
+  public double calcularHoraExtra(){
+    double hora_extra = this.quantidadeHorasExtras * (this.valorHora * 1.5);
+    return hora_extra;
+  }
+
+  public float calcularSalario(){
+    double valor_total = this.valorHora * this.quantidadeHoras + calcularHoraExtra();
+    this.valorSalario = (float) valor_total;
+    return (float) valor_total;
+  }
+
+  public void exibir(){
+    System.out.println("CODIGO FUNCIONARIO: " + this.codigo + 
+    "\nSalario do funcionario: " + calcularSalario() + 
+    "\nQuantidade de Horas trabalhadas: " + quantidadeHoras + 
+    "\nQuantidade de Horas extras trabal: " + quantidadeHorasExtras);
+    System.out.println();
+  }
+  
+
+}
