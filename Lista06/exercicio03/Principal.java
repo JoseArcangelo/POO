b0e772431dc921e057d3f4591e7442bf22a2623d
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-09-04 21:59:19 -0300

    Create Principal.java

---
+++ Lista06/exercicio03/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Jorginho");
+        p1.setIdade(20);
+        p1.setCpf("12345678-98");
+        p1.mostrar();
+
+        
+
+    }   
+}
