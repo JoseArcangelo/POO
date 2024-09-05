be14a1f84f04087fd7354d50c1432ab85071cee3
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-09-04 21:34:43 -0300

    Create Principal.java

---
+++ Lista06/exercicio01/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args){
+        Carro c1 = new Carro();
+        c1.setAno(2000);
+        c1.setMarca("Honda");
+        c1.setModelo("hb20");
+        c1.setPreco(40000);
+        c1.mostrar();
+    }   
+}
